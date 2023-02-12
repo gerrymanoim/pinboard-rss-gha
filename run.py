f863c568000d819b9f70dfc2ddb5f882b41b3cd4
gerrymanoim <gerrymanoim@gmail.com>
2023-02-12 16:51:31 -0500

    initi

---
+++ run.py
@@ -0,0 +1,39 @@
+import datetime as dt
+from bs4 import BeautifulSoup
+import requests
+from textwrap import dedent
+
+URL = "https://www.pinboard.in/popular/"
+
+
+def get_html() -> str:
+    r = requests.get(URL)
+    r.raise_for_status()
+    return r.text
+
+
+def process_link(title: str, link: str) -> str:
+    return f"<item><title>{title}</title><link>{link}</link><guid>{link}</guid></item>"
+
+
+def generate_feed() -> str:
+    soup = BeautifulSoup(get_html(), "html.parser")
+    links = "\n".join([
+        process_link(link.text, link["href"])
+        for link in soup.find_all("a", "bookmark_title")
+    ])
+    
+    return dedent(f"""
+    <rss version="2.0">
+        <channel>
+            <title>Pinboard Popular</title>
+            <link>{URL}</link>
+            <description>Pinboard popular</dscription>
+            {links}
+        </channel>
+    </rss>
+    """)
+
+
+if __name__ == "__main__":
+    print(generate_feed())
